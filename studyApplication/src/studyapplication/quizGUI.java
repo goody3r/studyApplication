af52b8147e4b301dc54b68999ced8a7b6324b293
Aidan <Aidan@10.0.0.179>
2021-03-23 19:04:50 -0400

    next btn

--- studyApplication/src/studyapplication/quizGUI.java
+++ studyApplication/src/studyapplication/quizGUI.java
@@ -1,5 +1,7 @@
 package studyapplication;
 
+import java.io.FileNotFoundException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +12,9 @@ package studyapplication;
  * @author harjafff
  */
 public class quizGUI extends javax.swing.JFrame {
+    
+        
+  
 
     private StartingMenu firstWindow;
 
